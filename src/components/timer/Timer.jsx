1a9ea88281b121486513b4cc391384a89c31fbba
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-03-11 09:39:26 +0000

    landing page updated

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -69,48 +69,12 @@ const Timer = () => {
   return (
     <div className="wrap-timer" id="home">
       <div className="headingCont">
-        <img className="eventlogo o1" src="assets/Eventtrans.png" alt="" />
-        <img className="eventlogo o2" src="assets/Eventtrans.png" alt="" />
-        <img className="eventlogo o3" src="assets/Eventtrans.png" alt="" />
+        <img className="eventlogo o1" src="assets/avinyalogo.png" alt="" />
+        <img className="eventlogo o2" src="assets/avinyalogo.png" alt="" />
+        <img className="eventlogo o3" src="assets/avinyalogo.png" alt="" />
       </div>
       {/* <h1 className="slogan">PUNE'S BIGGEST TECH CLASH</h1> */}
 
-      {hackathonStarted ? (
-        <>
-          <span className="startingSlogan">
-            Hey fellow hackers! The hackathon is live and buzzing with
-            creativity. Check out the awesome projects in action – your next
-            coding adventure awaits!{" "}
-          </span>
-          <div className="buttonCont">
-            <a
-              target="_blank"
-              href="https://shorturl.at/juvLP"
-              rel="noreferrer"
-            >
-              <button className="b1">Problem Statement</button>
-            </a>
-            {/* <a
-              target="_blank"
-              href="https://docs.google.com/forms/d/e/1FAIpQLScfqv5DpXq_rTAA-M__uAq4S8lUlgAN-_gZRdQk01Pdm-MkAg/viewform?usp=sf_link"
-              rel="noreferrer"
-            >
-              <button className="b1" disabled="true">
-                Project Submission
-              </button>
-            </a> */}
-            <button className="b2" disabled="true">
-              Project Submission
-            </button>
-            {/* <button className="b2" onClick={handleSubmissionButtonClick}>
-              Project Submission
-            </button> */}
-          </div>
-          {showProblems && <Problems />}
-          {showSubmission && <Submission />}
-        </>
-      ) : (
-        <>
           <h1 className="slogan">PUNE'S BIGGEST TECH CLASH</h1>
 
           <div className="timerContainer">
@@ -141,8 +105,7 @@ const Timer = () => {
               <button className="registerButton1">Participate Now</button>
             </a>
           </div>
-        </>
-      )}
+  
     </div>
   );
 };
