4fffc4165db93e9e9438d0a353ca4ab22e044b21
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-03-12 11:52:46 +0000

    Tracks UPdated

--- src/components/domain/Domain.jsx
+++ src/components/domain/Domain.jsx
@@ -9,37 +9,40 @@ const Domain = () => {
         </div>
         <div className="trackBoxes">
           <div className="trackBox">
-            <h5 className="trackHeading">WEB DEV</h5>
-            <p className="trackDesc">
-              For the Web Development enthusiasts, this hackathon is your canvas
-              to paint the digital world with your coding prowess. Dive into
-              front-end frameworks, back-end technologies, and create seamless,
-              user-friendly interfaces. Build websites and applications that not
-              only function flawlessly but also push the boundaries of what's
-              possible in the digital space.
-            </p>
+            <h5 className="trackHeading">Data Science</h5>
           </div>
           <div className="trackBox">
-            <h5 className="trackHeading">AIML</h5>
-            <p className="trackDesc">
-              In the AIML domain, sharpen your skills in crafting intelligent
-              algorithms and systems that mimic human-like learning. From
-              natural language processing to computer vision, the possibilities
-              are limitless. Seize this opportunity to explore the cutting-edge
-              advancements in AI and leave your mark on the landscape of
-              intelligent technologies.
-            </p>
+            <h5 className="trackHeading">Analog and Digital Communication</h5>
           </div>
           <div className="trackBox">
-            <h5 className="trackHeading">WEB 3.0</h5>
-            <p className="trackDesc">
+            <h5 className="trackHeading">Data Sceince</h5>
+          </div>
+          <div className="trackBox">
+            <h5 className="trackHeading">Signal and Image Processing</h5>
+          </div>
+          <div className="trackBox">
+            <h5 className="trackHeading">Real-world<br/> applications</h5>
+          </div>
+          <div className="trackBox">
+            <h5 className="trackHeading">Cyber Security</h5>
+          </div>
+          <div className="trackBox">
+            <h5 className="trackHeading">Data Visualization</h5>
+          </div>
+          <div className="trackBox">
+            <h5 className="trackHeading">Soft Computing & Artificial Intelligence</h5>
+          </div>
+
+          <div className="trackBox">
+            <h5 className="trackHeading">Data Sceince</h5>
+            {/* <p className="trackDesc">
               Now, brace yourselves for the frontier of technology with Web 3.0.
               The next evolution of the internet is here, promising
               decentralized, user-centric experiences. Explore blockchain, smart
               contracts, and decentralized applications (DApps) to revolutionize
               the way we interact online. This is your chance to be at the
               forefront of the internet's transformation.
-            </p>
+            </p> */}
           </div>
         </div>
       </div>
